6f3affe8b91d7fe8e6d7301628c27c062ce3d0a6
Daniel Page <dpage105@gmail.com>
2025-04-16 17:20:47 -0500

    Created new instance of BankAccount class in main method; Allowed user to deposit money and return balance.

---
+++ BankEncapsulation/ATM.cs
@@ -0,0 +1,6 @@
+namespace BankEncapsulation;
+
+public class ATM
+{
+    
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAccount();
+            double addFunds = 0;
+            double myBalance = 0;
+            
+            Console.WriteLine("Enter the amount of money you would like to deposit:");
+
+            while (!double.TryParse(Console.ReadLine(), out addFunds))
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            
+            myAccount.Deposit(addFunds);
+            myBalance = myAccount.GetBalance();
+            
+            Console.WriteLine($"Balance: {myBalance}");
         }
     }
 }
